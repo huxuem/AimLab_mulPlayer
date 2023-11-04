2f377e6c2c5d85aedca42c6f2a6564e58a55ae8a
Marktubzzy <310915462@qq.com>
2023-11-04 22:12:43 +0800

    增加枪口抖动和射击间隔

--- Assets/Scripts/Shoot/TargetShooter.cs
+++ Assets/Scripts/Shoot/TargetShooter.cs
@@ -7,26 +7,30 @@ public class TargetShooter : MonoBehaviour
     public int currentPlayerId = -1;
     public int curSide;
     [SerializeField] Camera cam;
+    [SerializeField] Animator animator;
 
     private Renderer renderer;
     [SerializeField] Material RedMat;
     [SerializeField] Material BlueMat;
 
-
+    private float timer;
 
     private void Start()
     {
+        timer=Time.time;
         renderer = GetComponent<Renderer>();
     }
 
     void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+    {   
+        
+        if(Input.GetMouseButtonDown(0)&& Time.time>timer+0.1f)
         {
             //Debug.Log("Click");
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
             if(Physics.Raycast(ray,out RaycastHit hit))
             {
+                animator.SetTrigger("Shoot");
                 Debug.Log(hit.collider.gameObject);
                 Target target = hit.collider.gameObject.GetComponent<Target>();
                 if(target != null )
@@ -35,6 +39,7 @@ public class TargetShooter : MonoBehaviour
                     target.Hit();
                 }
             }
+            timer = Time.time;
         }
     }
 
