cd8436ba915e4b9a977ca9de84c8851dbf5673d3
Marktubzzy <310915462@qq.com>
2023-11-07 19:32:09 +0800

    新增音效

--- Assets/Scripts/Shoot/TargetShooter.cs
+++ Assets/Scripts/Shoot/TargetShooter.cs
@@ -16,6 +16,7 @@ public class TargetShooter : MonoBehaviour
 
     public Cameraholder Cameraholder;
     public GameObject ready,gameover,newHits;
+    public AudioSource Shoot,ShootOpp,Shoot1;
 
     private float timer;
 
@@ -31,6 +32,7 @@ public class TargetShooter : MonoBehaviour
         if(Input.GetMouseButtonDown(0)&& Time.time>timer+0.1f)
         {
             //Debug.Log("Click");
+
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
             if(Physics.Raycast(ray,out RaycastHit hit))
             {
@@ -40,18 +42,23 @@ public class TargetShooter : MonoBehaviour
                 {
                     case "+":
                         Cameraholder.DPI += 0.1f;
+                        Shoot1.Play();
                         break;
                     case "-":
                         Cameraholder.DPI -= 0.1f;
+                        Shoot1.Play();
                         break;
                     case "++":
                         Cameraholder.DPI += 0.5f;
+                        Shoot1.Play();
                         break;
                     case "--":
                         Cameraholder.DPI -= 0.5f;
+                        Shoot1.Play();
                         break;
                     case "Ready£¿":
                         ready.SetActive(false);
+                        Shoot1.Play();
                         break;
                     case "Gameover":
                         Application.Quit();
@@ -59,11 +66,16 @@ public class TargetShooter : MonoBehaviour
                     case "Tryagain":
                         gameover.SetActive(false);
                         ready.SetActive(true);
+                        Shoot1.Play();
+                        break;
+                    case "Opp":
+                        ShootOpp.Play();
                         break;
                     default:
                         Target target = hit.collider.gameObject.GetComponent<Target>();
                         if (target != null)
                         {
+                            Shoot.Play();
                             Debug.Log("Hit!");
                             target.Hit();
                             GameObject spawnedHit=Instantiate(newHits, hit.point, Quaternion.identity);
